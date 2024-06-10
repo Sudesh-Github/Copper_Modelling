cfc6dded6e1a655766c3dd15a4a8acffe571f4dd
Sudesh-Github <102372827+Sudesh-Github@users.noreply.github.com>
2024-06-10 09:49:22 +0530

    Add files via upload

---
+++ Copper_copy.py
@@ -0,0 +1,205 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from animation import *
+from streamlit_lottie import st_lottie
+import matplotlib.animation as animation
+import datetime
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.preprocessing import StandardScaler,LabelEncoder
+from sklearn.svm import SVC
+import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+#page congiguration
+st.set_page_config(page_title= "Copper Modelling",
+                   page_icon= 'random',
+                   layout= "wide",)
+
+
+#=========hide the streamlit main and footer
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
+left,right=st.columns([1,3])
+
+with left:
+    url_link0="https://assets5.lottiefiles.com/packages/lf20_alg1vyxd.json"
+    st_lottie = lottie_home1(url_link0)
+
+with right:
+    st.markdown("<h1 style='text-align: center; color: red;'>COPPER SELLING PRICE PREDICTION AND STATUS</h1>",
+                unsafe_allow_html=True)
+
+    selected = option_menu(None, ['HOME',"SELLING PRICE PREDICTION","STATUS",],
+                           icons=["house",'cash-coin','trophy'],orientation='horizontal',default_index=0)
+
+    if selected=='HOME':
+        with left:
+            url_link1 = "https://assets1.lottiefiles.com/private_files/lf30_kit9njnq.json"
+            st_lottie = lottie_home1(url_link1)
+
+        st.write('## **WELCOME TO INDUSTRIAL COPPER MODELLING**')
+        st.markdown('##### ***This project focuses on modelling industrial copper data using Python and various libraries such as pandas, numpy, scikit-learn. The objective of the project is to preprocess the data, handle missing values, detect outliers, and handle skewness. Additionally, regression and classification models will be built to predict the selling price and determine if a sale was won or lost. The trained models will be saved as a pickle file for later use in a Streamlit application.***')
+        left ,right=st.columns([2,2])
+        with left:
+            st.write('### TECHNOLOGY USED')
+            st.write('- PYTHON   (PANDAS, NUMPY)')
+            st.write('- SCIKIT-LEARN')
+            st.write('- DATA PREPROCESSING')
+            st.write('- EXPLORATORY DATA ANALYSIS')
+            st.write('- STREAMLIT')
+
+        with right:
+            st.write("### MACHINE LEARNING MODEL")
+            st.write('#### REGRESSION - ***:red[EXTRATREEREGRESSOR]***')
+            st.write('- The ExtraTree Regressor is an ensemble learning method that belongs to the tree-based family of models.')
+            st.write('#### CLASSIFICATION - ***:green[RANDOMFORESTCLASSIFIER]***')
+            st.write('- The RandomForestClassifier is an ensemble learning method that combines multiple decision trees to create a robust and accurate classification model.')
+
+
+    
+    if selected == "SELLING PRICE PREDICTION":
+        url_link2 = "https://assets8.lottiefiles.com/packages/lf20_OPFirj1e4d.json"
+        st_lottie = lottie_price1(url_link2)
+        st.markdown("# :blue[Predicting Results based on Trained Model]")
+        # -----New Data inputs from the user for predicting the selling price-----
+        a1 = st.text_input("Quantity")
+        b1 = st.text_input("Status")
+        c1 = st.text_input("Item Type")
+        d1 = st.text_input("Application")
+        e1 = st.text_input("Thickness")
+        f1 = st.text_input("Width")
+        g1 = st.text_input("Country")
+        h1 = st.text_input("Customer")
+        i1 = st.text_input("Product Reference")
+                
+        @st.cache_resource
+        @st.cache_data
+        
+        def load_model():
+            with open('model.pkl', 'rb') as file:
+                    model = pickle.load(file)
+            return model
+
+        regression_model = load_model()
+        #st.write("Model loaded successfully.")
+
+        # -----Submit Button for PREDICT RESALE PRICE-----   
+        predict_button_1 = st.button("Predict Selling Price")
+
+        if predict_button_1:
+            try:
+
+                a1 = float(a1)
+                b1 = float(b1)
+                c1 = float(c1)
+                d1 = float(d1)
+                e1 = float(e1)
+                f1 = float(f1)
+                g1 = float(g1)
+                h1 = float(h1)
+                i1 = float(i1)
+
+                # -----Sending the user enter values for prediction to our model-----
+                new_sample_1 = np.array(
+                        [[np.log(a1), b1, c1, d1, np.log(e1), f1, g1, h1, i1]])
+                new_pred_1 = regression_model.predict(new_sample_1)[0]
+                # Function to process the input
+                    # Attempt to convert the input to float
+                
+                st.write(f'Predicted Selling Price : :green[₹] :green[{new_pred_1}]')
+            except ValueError:
+                # Catch ValueError and display a user-friendly error message
+                st.error("Invalid input: Please enter a valid number.")
+
+            st.info("The Predicted selling price may be differ from various reason like Supply and Demand Imbalances,Infrastructure and Transportation etc..",icon='ℹ️')
+
+
+
+    if selected=='STATUS':
+             #url_link3 = "https://assets9.lottiefiles.com/packages/lf20_lw4olqnf.json"
+             url_link3='https://assets8.lottiefiles.com/private_files/lf30_vsr6pvvl.json'
+             lottie_status1(url_link3)
+             url_link4= "https://assets1.lottiefiles.com/private_files/lf30_by9lgy8q.json"
+             lottie_status1(url_link4)
+
+             st.write(
+                    '##### ***<span style="color:yellow">Fill all the fields and Press the below button to view the status :red[WON / LOST] of copper in the desired time range</span>***',
+                    unsafe_allow_html=True)
+
+             cc1, cc2, cc3 = st.columns([2, 2, 2])
+             with cc1:
+                    quantity_cls = st.text_input('Enter Quantity  (Min:611728 & Max:1722207579) in tons')
+                    thickness_cls = st.text_input('Enter Thickness (Min:0.18 & Max:400)')
+                    width_cls= st.text_input('Enter Width  (Min:1, Max:2990)')
+
+             with cc2:
+                    selling_price_cls= st.text_input('Enter Selling Price  (Min:1, Max:100001015)')
+                    item_cls = st.text_input('Item Type')
+                    country_cls= st.text_input('Country Code')
+
+             with cc3:
+                    application_cls = st.text_input('Application Type')
+                    product_cls = st.text_input('Product Reference')
+                    item_delivery_date_cls = st.date_input("Estimated Delivery Date", datetime.date(2023,1, 1))
+
+                    reference_date = datetime.date(2023, 1, 1)
+                    days_since_reference = (item_delivery_date_cls - reference_date).days
+             with cc1:
+                    st.write('')
+                    st.write('')
+                    st.write('')
+                    if st.button('PREDICT STATUS'):
+
+                        @st.cache_resource
+                        def load_model():
+                            with open('classfier_model.pkl', 'rb') as file:
+                                    model = pickle.load(file)
+                            return model
+                        
+                        classification_model = load_model()
+
+                        data_cls = [
+                                quantity_cls,
+                                thickness_cls,
+                                width_cls,
+                                selling_price_cls,
+                                application_cls,
+                                product_cls,
+                                days_since_reference,
+                                item_cls,
+                                country_cls
+                                    ]
+
+
+                        x_cls = np.array(data_cls).reshape(1, -1)
+                        new_pred_2 = classification_model.predict(x_cls)
+                                
+                        if new_pred_2[0] ==1:
+                            st.write(f'Predicted Status : :green[WON]')
+                        else:
+                            st.write(f'Predicted Status : :red[LOST]')
+
+    st.info("The Predicted Status may be differ from various reason like Supply and Demand Imbalances,Infrastructure and Transportation etc..",icon='ℹ️')
+
+
+
+
